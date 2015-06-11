447a100ee0020c252b94e343ab919996939698b5
Rohini Rana <rohini.rana@blake.com.au>
2015-06-11 19:37:17 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Realdom::Application.config.secret_key_base = '1caed1470e4406b448e4b3a2ab634ff72aed1144d94e44493eb0ab8eed32a7d525abdca8aa74e44b9d28ee80b5f073a89e66064a932304d6dbef92c3520e732a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Realdom::Application.config.session_store :cookie_store, key: '_realdom_session'
